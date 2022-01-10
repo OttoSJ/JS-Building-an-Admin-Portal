f6ad05cc95f2ff171bc787779d07ffb9eaf0daf1
Otto Jones <ottosjonesjr@gmail.com>
2022-01-09 21:46:23 -0500

    added buttons, input fields, book titles, and the functionality to update customer pages book count

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,43 @@
 
-// Your Code Here
+
+async function getData() {
+    let response = await fetch("http://localhost:3001/listBooks")
+    let books = await response.json()
+
+    books.map(renderData)
+
+}
+
+function renderData(book) {
+    let mainDiv = document.getElementById("root")
+    
+    let li = document.createElement("li")
+    li.textContent = book.title
+
+    let input = document.createElement("input")
+    input.type = "text"
+    input.value = book.quantity
+
+    let button = document.createElement("button")
+    button.textContent = "Submit"
+
+    button.addEventListener("click", () => {
+        fetch("http://localhost:3001/updateBook",{
+            method: "PATCH", 
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "id": book.id,
+                "quantity": input.value
+
+            })
+         })
+    })
+
+    li.append(input, button)
+    mainDiv.append(li)
+}
+
+
+getData()
